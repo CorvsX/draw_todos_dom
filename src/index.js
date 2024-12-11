082cee6025a5735a7caf8e221d03f586e8280e56
Zeida <zeidaromen@outlook.es>
2024-12-11 17:43:07 +0000

    🔄 Actualización de la aplicación de tareas
    ✅ Se ha agregado un nuevo método  en  para mostrar todas las tareas
    📈 Se ha actualizado el archivo  para implementar la lógica del nuevo método
    📄 Se ha agregado un nuevo botón en  para mostrar todas las tareas
    🎨 Se ha actualizado el archivo  para mejorar la apariencia y el diseño del nuevo botón
    💻 Se ha mejorado la funcionalidad y la usabilidad de la aplicación

--- src/index.js
+++ src/index.js
@@ -452,22 +452,28 @@ const todos = [
 ];
 const contenedor = document.querySelector(".tasks-container");
 todos.forEach(task => {
-    contenedor.innerHTML += printTask(task);
+  contenedor.innerHTML += printTask(task);
 });
 
 function filter() {
   const user = parseInt(document.querySelector(".input-filter").value);
   const filteredTasks = todos.filter(task => task.userId === user);
   contenedor.innerHTML = "";
-  console.log(filteredTasks);
   filteredTasks.forEach(task => {
-      contenedor.innerHTML += printTask(task);
+    contenedor.innerHTML += printTask(task);
   });
 }
 
-function printTask(task){
-  const todo= "🚧";
-  const done= "✅";
-  const taskHtml= `<div class="task"><div class="user-id">${task.userId}</div><div class="task-text ${task.completed? "done":"todo"}"> ${task.completed?done:todo} ${task.title}</div></div>`
-  return taskHtml;
+function printTask(task) {
+  const todo = "🚧";
+  const done = "✅";
+  return `<div class="task"><div class="user-id">${task.userId}</div><div class="task-text ${task.completed ? "done" : "todo"}"> ${task.completed ? done : todo} ${task.title}</div></div>`;
+}
+
+function showAll() {
+  const contenedor = document.querySelector(".tasks-container");
+  contenedor.innerHTML = "";
+  todos.forEach(task => {
+    contenedor.innerHTML += printTask(task);
+  });
 }
