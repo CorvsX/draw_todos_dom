b763c844aadae248bf794197fd74d81a8debd189
CsX <alexxtrabajosvfx@gmail.com>
2024-12-12 19:13:17 +0000

    Añadimos el DELETE Y POST

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ function filter() {
 function printTask(task) {
   const todo = "🚧";
   const done = "✅";
-  return `<div class="task"><div class="user-id">${task.userId}</div><div class="task-text ${task.completed ? "done" : "todo"}"> ${task.completed ? done : todo} ${task.title}</div></div>`;
+  return `<div class="task"><div class="user-id">${task.userId}</div><div class="task-text ${task.completed ? "done" : "todo"}"> ${task.completed ? done : todo} ${task.title}</div><button id="${task.userId}" onclick="deleteTask(${task.id})">🗑️</button></div>`;
 }
 
 function showAll(todos) {
@@ -31,4 +31,48 @@ function showAll(todos) {
   todos.forEach(task => {
     contenedor.innerHTML += printTask(task);
   });
+}
+
+function addTask() {
+  const idUser = document.getElementById("id-user").value;
+  const msgTask = document.getElementById("task-info").value;
+  const checkboxDone = document.getElementById("done").checked;
+
+  const newTask =
+  {
+    userId: idUser,
+    title: msgTask,
+    completed: checkboxDone
+  };
+
+  fetch(urlAPITodo, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(newTask)
+  })
+    .then(response => response.json())
+    .then(task => {
+      // printTask(task);
+      todos.push(task);
+      showAll(todos);
+      console.log(task);
+    });
+}
+
+function deleteTask(id) {
+  fetch(`${urlAPITodo}/${id}`, {
+    method: "DELETE"
+  })
+    .then(response => {
+      if (response.ok) {
+        console.log("Tarea eliminada");
+        todos = todos.filter(task => task.id !== id);
+        showAll(todos);
+      } else {
+        console.error("La tarea no ha sido eliminada");
+      }
+    })
+    .catch(error => console.error("Error", error));
 }
